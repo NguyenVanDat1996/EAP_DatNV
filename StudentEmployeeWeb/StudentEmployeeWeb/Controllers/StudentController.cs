ca23dcbc73634b12fbc3a4e78cbdddfecb796567
nvdatbn <zenkura012@gmail.com>
2017-10-24 13:06:59 +0700

    Prac

---
+++ StudentEmployeeWeb/StudentEmployeeWeb/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using StudentEmployeeWeb.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentEmployeeWeb.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        StudentServiceClient client = new StudentServiceClient();
+        public ActionResult Index()
+        {
+            return View(client.getAllStudent().ToList());
+        }
+        
+    }
+}
---
+++ StudentEmployeeWeb/StudentEmployeeWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentEmployeeWeb.Startup))]
+namespace StudentEmployeeWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
